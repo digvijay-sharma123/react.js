9b4a0e75f24b3f33971389710217ec01178ecf83
digvijay-sharma123 <dk832036@student.nitw.ac.in>
2024-09-19 20:25:19 +0530

    initial commit

---
+++ reactjs/my-app/src/App.js
@@ -0,0 +1,12 @@
+import Chai from './chai'
+
+function App() {
+  return (
+    <>
+      <h1>Jai Shree Krishna</h1>
+      <Chai />
+    </>
+  );
+}
+
+export default App;
---
+++ reactjs/my-app/src/chai.js
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h1>Jai Shree Rama</h1>
+    )
+}
+
+export default Chai;
