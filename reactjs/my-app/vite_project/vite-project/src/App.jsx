7fa4b284696745c7fe564b8a5c4bba039a179270
digvijay-sharma123 <dk832036@student.nitw.ac.in>
2024-09-21 17:54:50 +0530

    first commit

---
+++ reactjs/my-app/vite_project/vite-project/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [cnt, setCnt]= useState(15)
+  // let cnt= 15;
+  const addValue =()=>{
+    if(cnt>=20) return cnt;
+    cnt+=1
+    setCnt(cnt)
+  }
+  // Hook yaha padhe mainly ui rendering ke liye use krte
+  const removeValue =()=>{
+    if(cnt<=0) return cnt;
+    cnt-=1
+    setCnt(cnt)
+  }
+
+  return (
+    <>
+       <h1>jai shree Krishna</h1>
+       <h2>counter value : {cnt} </h2>
+       <button onClick={addValue}>Add Value</button>
+       <br />
+       <br />
+       <button onClick= {removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
