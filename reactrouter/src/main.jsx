f01ed946020ab1d10f5d856fa2cb86adaeb278b3
digvijay-sharma123 <dk832036@student.nitw.ac.in>
2024-09-26 17:14:27 +0530

    First commit

---
+++ reactrouter/src/main.jsx
@@ -0,0 +1,37 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import About from './components/About/about.jsx'
+import Home from './components/Home/home.jsx'
+import Contact from './components/Contact/Contact.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "about",
+        element: <About/>
+      },
+      {
+        path: "contact",
+        element: <Contact/>
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
