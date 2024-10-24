6df07f8b688a8bc66f24aadf0ed93ec7b09623b7
digvijay-sharma123 <dk832036@student.nitw.ac.in>
2024-10-24 22:27:24 +0530

    Theme Switcher

---
+++ themeswitcher/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust these paths based on your project structure
+    './public/index.html',
+  ],
+  darkMode:"class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
