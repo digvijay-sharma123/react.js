226ee3d726297060d6ac8b9da337b932ba21a62a
digvijay-sharma123 <dk832036@student.nitw.ac.in>
2025-02-26 20:38:00 +0530

    event and state handle

---
+++ React_revision/react_revision/src/components/TextForm.js
@@ -0,0 +1,23 @@
+import React,{useState} from 'react'
+export default function TextForm(props) {
+    const [text,settext] = useState('')
+    const onhandleupclick = ()=>{
+        console.log("Upper Case Clicked")
+        settext("you have clicked on handleupclick")
+    }
+    const onchanged = (event)=>{
+        console.log("on change button clicked")
+        settext(event.target.value)
+    }
+    return (
+        <div>
+            <h1>{props.heading}</h1>
+            <form>
+                <div className="mb-3">
+                    <textarea className='form-control' value = {text} onChange={onchanged} id = "my-box" rows = "5"></textarea>
+                </div>
+            </form>
+            <button className='btn btn-primary' onClick={onhandleupclick}>Convert to Upper Case</button>
+        </div>
+    )
+}
