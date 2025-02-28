22feb4ba0f35efac9bc7590ac1c9960ddb74455e
digvijay-sharma123 <dk832036@student.nitw.ac.in>
2025-02-28 09:26:58 +0530

    first commit

--- React_revision/react_revision/src/components/Navbar.js
+++ React_revision/react_revision/src/components/Navbar.js
@@ -23,7 +23,7 @@ function Navbar(props) {
             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
             <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
           </form> */}
-          <div className={`form-check form-switch text-${props.mode==='light'? 'dark': 'light'}`}>
+          <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
             <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
           </div>
