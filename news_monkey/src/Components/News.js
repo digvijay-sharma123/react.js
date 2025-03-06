22abb39f5b93f193c01837045072302550f9b4ce
digvijay-sharma123 <dk832036@student.nitw.ac.in>
2025-03-06 08:47:27 +0530

    initial commit

--- news_monkey/src/Components/News.js
+++ news_monkey/src/Components/News.js
@@ -3,42 +3,66 @@ import Newsitem from './Newsitem'
 export default class News extends Component {
     constructor() {
         super();
-        console.log("i am constructor")
         this.state = {
             articles: [],
-            loading: false
+            loading: false,
+            page: 1,
         }
     }
 
-    async componentDidMount(){
-        console.log("cdm")
-        let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=38e082c5daaa4c14a68b2e5e9158b01e"
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=38e082c5daaa4c14a68b2e5e9158b01e&page=1"
         let data = await fetch(url)
         let parseddata = await data.json()
         console.log(parseddata)
-        this.setState({articles: parseddata.articles})
+        this.setState({ articles: parseddata.articles, totalArticles: parseddata.totalResults})
     }
+
+    handleNextPage = () => {
+        if(this.state.page+1>Math.ceil(this.state.totalArticles/20)){
+
+        }
+        else
+           this.setState({ page: this.state.page + 1 }, this.fetchNews);
+    };
+
+    handlePreviousPage = () => {
+        this.setState({ page: this.state.page - 1 }, this.fetchNews);
+    };
+
+    fetchNews = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=38e082c5daaa4c14a68b2e5e9158b01e&page=${this.state.page}`;
+        let data = await fetch(url);
+        let parseddata = await data.json();
+        this.setState({ articles: parseddata.articles });
+    };
+
 
     render() {
         return (
-            <div className='container my-3'>
-                <h2>NewsMonkey - Top Headlines</h2>
-                <div className="row">
-                    {this.state.articles.map((element) => {
-                        return (
-                            <div className='col-md-4' key={element.url}>
-                                <Newsitem
-                                    title={element.title ? element.title:" "}
-                                    desc={element.description ? element.description:" "}
-                                    imageurl={element.urlToImage}
-                                    newsurl={element.url}
-                                />
-                            </div>
-                        );
-                    })}
+            <>
+                <div className='container my-3'>
+                    <h2>NewsMonkey - Top Headlines</h2>
+                    <div className="row">
+                        {this.state.articles.map((element) => {
+                            return (
+                                <div className='col-md-4' key={element.url}>
+                                    <Newsitem
+                                        title={element.title ? element.title : " "}
+                                        desc={element.description ? element.description : " "}
+                                        imageurl={element.urlToImage}
+                                        newsurl={element.url}
+                                    />
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
-
-            </div>
+                <div className='container d-flex justify-content-between'>
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark my-2" onClick={this.handlePreviousPage}>&larr; Prev</button>
+                    <button type="button" className="btn btn-dark my-2" onClick={this.handleNextPage}>Next &rarr;</button>
+                </div>
+            </>
         )
     }
 }
--- news_monkey/src/Components/Newsitem.js
+++ news_monkey/src/Components/Newsitem.js
@@ -2,19 +2,20 @@ import React, { Component } from 'react'
 
 export default class Newsitem extends Component {
     render() {
-        let {title,description,imageurl,newsurl} = this.props
+        let { title, description, imageurl, newsurl } = this.props
         return (
-            <div>
-                <div className="card" style={{width: "18rem"}}>
-                    <img src={imageurl} className="card-img-top" alt="..."/>
+            <>
+                <div>
+                    <div className="card" style={{ width: "18rem" }}>
+                        <img src={imageurl} className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">{description}</p>
-                            <a href={newsurl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">Read More</a>
+                            <a href={newsurl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark">Read More</a>
                         </div>
+                    </div>
                 </div>
-
-            </div>
+            </>
         )
     }
 }
